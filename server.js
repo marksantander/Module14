bb7801e68ea35806d9e0d8ec47a0d130af80c01f
marksantander <msntndr.ms@gmail.com>
2023-09-24 23:01:43 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const handlebars = require('handlebars');
+const sequelize = require('sequelize');
+const app = express();
+
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+const db = sequelize.connect('mysql://localhost:3306/blog_site', {
+  username: 'root',
+  password: 'root',
+});
+
+const Post = db.define('post', {
+  title: {
+    type: sequelize.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: sequelize.TEXT,
+    allowNull: false,
+  },
+});
+
+app.get('/', async (req, res) => {
+  const posts = await Post.findAll();
+  res.render('index', { posts });
+});
+
+app.get('/posts/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render('post', { post });
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
